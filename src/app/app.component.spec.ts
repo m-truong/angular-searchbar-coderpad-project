d5bff2c1a225114c35cb5d5f4c61e53d4747110e
Michael Truong <truong.michael.2013@gmail.com>
2023-08-14 16:05:15 -0400

    Initial commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'angular-searchbar-coderpad-project'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('angular-searchbar-coderpad-project');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-searchbar-coderpad-project app is running!');
+  });
+});
---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,19 @@
+// INSTRUCTIONS:
+/**
+ * 1. build an angular search bar component 
+ * 2. it should take users input 
+ * 3. then it should send requests to GifyApi
+ * 4. and it should return those images being displayed in a Grid
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss']
+})
+export class SearchBarComponent {
+
+  
+}
