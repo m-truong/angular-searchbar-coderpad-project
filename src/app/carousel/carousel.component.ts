e22f0822c652ea686d813a2fe0e070b42dc53cd3
Michael Truong <truong.michael.2013@gmail.com>
2023-08-26 19:53:05 -0400

    Remove SimpleChanges from CarouselComponent

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,31 +1,22 @@
 import {
 	Component,
 	Input,
-	OnChanges,
 	OnInit,
-	SimpleChanges,
 } from '@angular/core';
 import { SearchBarService } from '../services/search-bar.service';
-import { logger, loggerCommand } from '../shared/utils/common-functions';
 
 @Component({
 	selector: 'app-carousel',
 	templateUrl: './carousel.component.html',
 	styleUrls: ['./carousel.component.scss'],
 })
-export class CarouselComponent implements OnInit, OnChanges {
+export class CarouselComponent implements OnInit {
 
-	// internal state
 	giphyImageURL: any;
 
 	constructor(private searchBarService: SearchBarService) {}
 
 	ngOnInit(): void {
-		// this.attachGiphyImg();
-	}
-
-	ngOnChanges(changes: SimpleChanges): void {
-		this.attachGiphyImg();
 	}
 
 	attachGiphyImg() {
