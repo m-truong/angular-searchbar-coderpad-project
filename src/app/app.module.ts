c0e734aa98275fe26f021977e719dfb9a1c39201
Michael Truong <truong.michael.2013@gmail.com>
2023-09-11 15:17:51 -0400

    Remove unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,9 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-// import { AnimationBuilder } from '@angular/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { SearchBarComponent } from './search-bar/search-bar.component';
 import { SearchBarService } from './services/search-bar.service';
 import { GiphyComponent } from './giphy/giphy.component';
 import { SearchBarModule } from './search-bar/search-bar.module';
